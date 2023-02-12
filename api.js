d1eb354ea5167cd5fc9c231829ef5395c94837ec
BuchPawares <Pawares4011@gmail.com>
2023-02-12 10:11:50 +0700

    Update api.js

--- api.js
+++ api.js
@@ -13,7 +13,7 @@ function getTxt() {
 function showData(data){
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${data.name}&units=metric&cnt=40&appid=a5551a799c6cda11fc25322be201b948&lang=th`)
     .then(re => re.json())
-    //.then(dataWe => showTast(dataWe))
+    .then(dataWe => showTast(dataWe))
     .then(dataWe => console.log(dataWe))
     document.getElementById("demo").innerHTML =`${data.name}`;
     document.getElementById("demo1").innerHTML =`${data.main.temp_min}°C`;
@@ -26,12 +26,12 @@ function showTast(dataWe){
     document.getElementById("cwe-2").innerHTML =`${dataWe.city.name}`;
     document.getElementById("cwe-3").innerHTML =`${dataWe.city.name}`;
     document.getElementById("cwe-4").innerHTML =`${dataWe.city.name}`;
-    document.getElementById("cw-t-1").innerHTML =`${dataWe.list[5].main.temp_min}°C`;
-    document.getElementById("cw-t-2").innerHTML =`${dataWe.list[10].main.temp_min}°C`;
-    document.getElementById("cw-t-3").innerHTML =`${dataWe.list[30].main.temp_min}°C`;
-    document.getElementById("cw-t-4").innerHTML =`${dataWe.list[38].main.temp_min}°C`;
-    document.getElementById("img-1").src =`http://openweathermap.org/img/wn/${dataWe.list[5].weather[0].icon}@2x.png`;
-    document.getElementById("img-2").src =`http://openweathermap.org/img/wn/${dataWe.list[10].weather[0].icon}@2x.png`;
-    document.getElementById("img-3").src =`http://openweathermap.org/img/wn/${dataWe.list[30].weather[0].icon}@2x.png`;
-    document.getElementById("img-4").src =`http://openweathermap.org/img/wn/${dataWe.list[38].weather[0].icon}@2x.png`;
+    document.getElementById("cw-t-1").innerHTML =`${dataWe.list[11].main.temp_min}°C`;
+    document.getElementById("cw-t-2").innerHTML =`${dataWe.list[19].main.temp_min}°C`;
+    document.getElementById("cw-t-3").innerHTML =`${dataWe.list[27].main.temp_min}°C`;
+    document.getElementById("cw-t-4").innerHTML =`${dataWe.list[35].main.temp_min}°C`;
+    document.getElementById("img-1").src =`http://openweathermap.org/img/wn/${dataWe.list[11].weather[0].icon}@2x.png`;
+    document.getElementById("img-2").src =`http://openweathermap.org/img/wn/${dataWe.list[19].weather[0].icon}@2x.png`;
+    document.getElementById("img-3").src =`http://openweathermap.org/img/wn/${dataWe.list[27].weather[0].icon}@2x.png`;
+    document.getElementById("img-4").src =`http://openweathermap.org/img/wn/${dataWe.list[35].weather[0].icon}@2x.png`;
 }
