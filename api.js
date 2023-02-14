42b604369aba102b71d6a9df5852c04c7fecbfbf
BuchPawares <Pawares4011@gmail.com>
2023-02-14 22:10:03 +0700

    Update api.js

--- api.js
+++ api.js
@@ -41,8 +41,8 @@ function dt(dataWe){
     document.getElementById("dw-2").innerHTML =`${dataWe.list[19].weather[0].description}`;
     document.getElementById("dw-3").innerHTML =`${dataWe.list[27].weather[0].description}`;
     document.getElementById("dw-4").innerHTML =`${dataWe.list[35].weather[0].description}`;
-    document.getElementById("v-1").innerHTML =`${dataWe.list[11].visibility}`;
-    document.getElementById("v-2").innerHTML =`${dataWe.list[19].visibility}`;
-    document.getElementById("v-3").innerHTML =`${dataWe.list[27].visibility}`;
-    document.getElementById("v-4").innerHTML =`${dataWe.list[35].visibility}`;
+    document.getElementById("v-1").innerHTML =`ความชื้น ${dataWe.list[11].main.humidity}%`;
+    document.getElementById("v-2").innerHTML =`ความชื้น ${dataWe.list[19].main.humidity}%`;
+    document.getElementById("v-3").innerHTML =`ความชื้น ${dataWe.list[27].main.humidity}%`;
+    document.getElementById("v-4").innerHTML =`ความชื้น ${dataWe.list[35].main.humidity}%`;
 }
