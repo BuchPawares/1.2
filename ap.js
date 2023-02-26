105e276fbf158116d66619ed0a2c14625bb6287b
BuchPawares <Pawares4011@gmail.com>
2023-02-26 22:41:58 +0700

    Update ap.js

--- ap.js
+++ ap.js
@@ -1,26 +1,21 @@
 set();
 function set(){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Bangkok&units=metric&appid=a5551a799c6cda11fc25322be201b948&lang=th`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Bangkok&units=metric&appid=a5551a799c6cda11fc25322be201b948&lang=th`)
     .then(response => response.json())
     //.then(data => console.log(data))
-    .then(data => showData(data));
+    .then(PM => showPM(PM));
 
 }
 function getTxt() {
     var txt = document.getElementById("txt").value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${txt}&units=metric&appid=a5551a799c6cda11fc25322be201b948&lang=th`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${txt}&units=metric&appid=a5551a799c6cda11fc25322be201b948&lang=th`)
     .then(response => response.json())
-    .then(data => showData(data))
+    .then(PM => showPM(PM))
     //.then(data => console.log(data))
 } 
-function showData(data){
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${data.name}&units=metric&cnt=40&appid=a5551a799c6cda11fc25322be201b948&lang=th`)
-    .then(re => re.json())
-    .then(dataWe => showTast(dataWe))
-    //.then(dataWe => console.log(dataWe))  
-}
-function showPM(dataWe){
-    fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${dataWe.city.coord.lat}&lon=${dataWe.city.coord.lon}&appid=a5551a799c6cda11fc25322be201b948&lang=th`)
+
+function showPM(PM){
+    fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${PM.city.coord.lat}&lon=${PM.city.coord.lon}&appid=a5551a799c6cda11fc25322be201b948&lang=th`)
     .then(re => re.json())
     .then(dataW => showP(dataW));
 } 
