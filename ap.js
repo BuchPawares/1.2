edffec06b02deb522907f1591a9fa3f55c152481
BuchPawares <Pawares4011@gmail.com>
2023-02-26 22:50:40 +0700

    a

--- ap.js
+++ ap.js
@@ -15,8 +15,8 @@ function getTxt() {
 } 
 
 function showPM(PM){
-    var api = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${PM.city.coord.lat}&lon=${PM.city.coord.lon}&appid=a5551a799c6cda11fc25322be201b948&lang=th`;
-    fetch(api)
+    var api = 
+    fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${PM.city.coord.lat}&lon=${PM.city.coord.lon}&appid=a5551a799c6cda11fc25322be201b948&lang=th`)
     .then(re => re.json())
     .then(dataW => showP(dataW));
 } 
